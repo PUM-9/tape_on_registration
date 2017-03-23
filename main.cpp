409dda0c2b074004ee33c82e7d82ff2127d57936
Ubuntu i rummet <nej@nej>
2017-03-23 16:26:18 +0100

    gustav left computer without commiting andpushing his work so... lol

--- main.cpp
+++ main.cpp
@@ -10,19 +10,19 @@ typedef float millimeter;
 
 int main() {
     
-    millimeter width = 45.0;
-    millimeter height = 30.0;
-    millimeter depth = 30.0;
-    millimeter step_size = 0.1;
+    millimeter depth = 460.0;
+    millimeter height = 430.0;
+    millimeter width = 430.0;
+    millimeter step_size = 0.5;
     Cloud cube = Cloud();
 
     // Create two xy planes in the xzy space.
-    for (millimeter i=0; i<width; i+=step_size) {
+    for (millimeter i=400; i<width; i+=step_size) {
 
-        for (millimeter j=0; j<height; j+=step_size) {
+        for (millimeter j=400; j<height; j+=step_size) {
             //Add one point for each side of the cube
-            Point point1 = Point(i, j, 0);
-            Point point2 = Point(i, j, depth);
+            Point point1 = Point(i, j, -400);
+            Point point2 = Point(i, j, -depth);
             cube.push_back(point1);
             cube.push_back(point2);
         }
@@ -30,11 +30,11 @@ int main() {
     }
 
     // Create two yz planes in the xyz space.
-    for (millimeter i=0; i<depth; i+=step_size) {
+    for (millimeter i=400; i<depth; i+=step_size) {
 
-        for (millimeter j=0; j<height; j+=step_size) {
-            Point point1 = Point(0, j, i);
-            Point point2 = Point(width, j, i);
+        for (millimeter j=400; j<height; j+=step_size) {
+            Point point1 = Point(400, j, -i);
+            Point point2 = Point(width, j, -i);
             cube.push_back(point1);
             cube.push_back(point2);
         }
@@ -42,11 +42,11 @@ int main() {
     }
 
     // Create two xz planes in the xyz space.
-    for (millimeter i=0; i<depth; i+=step_size) {
+    for (millimeter i=400; i<depth; i+=step_size) {
 
-        for (millimeter j=0; j<width; j+=step_size) {
-            Point point1 = Point(j, 0, i);
-            Point point2 = Point(j, height, i);
+        for (millimeter j=400; j<width; j+=step_size) {
+            Point point1 = Point(j, 400, -i);
+            Point point2 = Point(j, height, -i);
             cube.push_back(point1);
             cube.push_back(point2);
         }
